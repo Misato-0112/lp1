e6f0342944f36fe95c9a63369cacd32c15bde183
Misato-0112 <misato@m0112.mygbiz.com>
2024-10-01 00:21:15 -0500

    10.01最終プッシュ

--- assets/script/main.js
+++ assets/script/main.js
@@ -16,13 +16,11 @@ $(document).ready(function () {
 
   // タブ切り替え
   $(".tab-button").click(function () {
-    // アクティブなタブのスタイルを更新
     $(".tab-button").removeClass("active");
     $(this).addClass("active");
 
-    // 対象のタブコンテンツをフェードイン・フェードアウトで切り替え
     var target = $(this).data("target");
-    $(".tab-content").fadeOut(200); // フェードアウト（200ms）
-    $("#" + target).fadeIn(200); // フェードイン（200ms）
+    $(".tab-content").fadeOut(200);
+    $("#" + target).fadeIn(200);
   });
 });
